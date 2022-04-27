56a7c28e3286c97182bad9cf615f2845c6b05d5f
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 21:30:48 -0500

    3) Creando Fizzbuzz service

--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,6 +1,7 @@
 // Será el reemplazo de app-legado.js
 const Reader = require('./lib/utils/Reader')
 const ExplorerService = require('./lib/services/ExplorerServices')
+const FizzbuzzService = require('./lib/services/FizzbuzzService')
 
 const explorers = Reader.readJsonFile("explorers.json")
 
@@ -8,3 +9,4 @@ ExplorerService.filterByMission(explorers, "node")
 ExplorerService.getAmountOfExplorersByMission(explorers, "node")
 ExplorerService.getExplorersUsernamesByMission(explorers, "node")
 
+FizzbuzzService.applyValidationInExplorer(explorer1)
