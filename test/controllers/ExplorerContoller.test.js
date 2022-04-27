dc4ed73ac427ff67af3671b23be181d7154724b0
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-27 17:57:42 -0500

    23) Agregando test controller: Get explorers array lenght

--- test/controllers/ExplorerContoller.test.js
+++ test/controllers/ExplorerContoller.test.js
@@ -7,10 +7,15 @@ describe("Test for ExplorerContoller", () => {
     });
 
     test("2) Test: Get Explorers usernames by Mission", () => {
-      const ExplorersUsernames = ExplorerController.getExplorersUsernamesByMission("node");
-      const match = ExplorersUsernames.some(el => {
-        return el.includes('ajolonauta') ? true : false
-      })
-      expect(match).toBe(true)
-    })
+        const ExplorersUsernames = ExplorerController.getExplorersUsernamesByMission("node");
+        const match = ExplorersUsernames.some(el => {
+            return el.includes("ajolonauta") ? true : false;
+        });
+        expect(match).toBe(true);
+    });
+
+    test("3) Test: Get Explorers amount", () => {
+        const ExplorersLenght = ExplorerController.getExplorersAmonutByMission("node");      
+        expect(ExplorersLenght).toBe(10);
+    });
 });
