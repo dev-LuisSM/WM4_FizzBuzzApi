fb9c0ceaf36ef1fb88f4848a85347f3571829e4f
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-27 11:36:51 -0500

    5) Agregando test: Reader

---
+++ fizzbuzz/test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+// const Reader = require('../../lib/utils/Reader')
+
+describe("Test for Reader Service", () => {
+  test("Return a list of explorers", () => {
+    // const explorers = Reader.readJsonFile('../../explorers.json') // Can't find the path 
+    expect(1).toBe(1);
+  });
+});
