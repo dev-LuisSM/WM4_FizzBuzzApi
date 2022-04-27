29dc5a319445e6347c189eb212b16e204886e081
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-26 21:16:01 -0500

    2) Creando e importando ExplorerServices

--- fizzbuzz/app-legado.js
+++ fizzbuzz/app-legado.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
-// Part 1 Read json file ===========================
-// const rawdata = fs.readFileSync("explorers.json");
-// const explorers = JSON.parse(rawdata);
+// ✅ Part 1 Read json file ===========================
+const rawdata = fs.readFileSync("explorers.json");
+const explorers = JSON.parse(rawdata);
 
-// Part 2: Get the quantity of explorers names in node
+// ✅ Part 2: Get the quantity of explorers names in node 
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 // console.log(explorersInNode.length)
 
-// Part4: Get the explorer's usernames in Node
+// ✅ Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
 //console.log(usernamesInNode)
--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,4 +1,10 @@
+// Será el reemplazo de app-legado.js
 const Reader = require('./lib/utils/Reader')
+const ExplorerService = require('./lib/services/ExplorerServices')
 
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(explorers)
+
+ExplorerService.filterByMission(explorers, "node")
+ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+
