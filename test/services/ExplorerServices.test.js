b7421c105ef3579f9197adf07e72bad165d54456
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-27 14:00:46 -0500

    17) Agregando y corrigiendo errores con Linter

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -1,21 +1,21 @@
-const ExplorerService = require('./../../lib/services/ExplorerServices')
+const ExplorerService = require("./../../lib/services/ExplorerServices");
 
-describe('Test for ExplorerService', () => {
-  test('1) Test: Filter by Mission', () => {
-    const explorers = [{ mission: "node" }]
-    const ExplorerByMission = ExplorerService.filterByMission(explorers, "node")
-    expect(ExplorerByMission.length).toBe(1)
-  })
+describe("Test for ExplorerService", () => {
+    test("1) Test: Filter by Mission", () => {
+        const explorers = [{ mission: "node" }];
+        const ExplorerByMission = ExplorerService.filterByMission(explorers, "node");
+        expect(ExplorerByMission.length).toBe(1);
+    });
 
-  test('2) Test: Filter by Mission', () => {
-    const explorers = [{ mission: "node" }]
-    const ExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
-    expect(ExplorerInNode).toBe(1)
-  })
+    test("2) Test: Filter by Mission", () => {
+        const explorers = [{ mission: "node" }];
+        const ExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(ExplorerInNode).toBe(1);
+    });
 
-  test('3) Test: Get Username by Mission', () => {
-    const explorers = [{ mission: "node", githubUsername: "Luis" }]
-    const ExplorerUsername = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
-    expect(ExplorerUsername[0]).toBe('Luis')
-  })
-})
+    test("3) Test: Get Username by Mission", () => {
+        const explorers = [{ mission: "node", githubUsername: "Luis" }];
+        const ExplorerUsername = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(ExplorerUsername[0]).toBe("Luis");
+    });
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,28 +1,28 @@
-const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
-describe('Test for FizzbuzzService', () => {
-  test('1) Fizzbuss 1st case: trick = score', () => {
-    const explorer = { name: "Explorer1", score: 1 }
-    const service = FizzbuzzService.applyValidationInExplorer(explorer)
-    expect(service.trick).toBe(service.score) // 1
-  })
+describe("Test for FizzbuzzService", () => {
+    test("1) Fizzbuss 1st case: trick = score", () => {
+        const explorer = { name: "Explorer1", score: 1 };
+        const service = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(service.trick).toBe(service.score); // 1
+    });
 
-  test('2) Fizzbuss 2nd case: trick = fizz', () => {
-    const explorer = { name: "Explorer3", score: 3 }
-    const service = FizzbuzzService.applyValidationInExplorer(explorer)
-    expect(service).toBe('FIZZ') // FIZZ
-  })
+    test("2) Fizzbuss 2nd case: trick = fizz", () => {
+        const explorer = { name: "Explorer3", score: 3 };
+        const service = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(service).toBe("FIZZ"); // FIZZ
+    });
 
-  test('3) Fizzbuss 3rd case: trick = buzz', () => {
-    const explorer = { name: "Explorer5", score: 5 }
-    const service = FizzbuzzService.applyValidationInExplorer(explorer)
-    expect(service).toBe('BUZZ') // BUZZ
-  })
+    test("3) Fizzbuss 3rd case: trick = buzz", () => {
+        const explorer = { name: "Explorer5", score: 5 };
+        const service = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(service).toBe("BUZZ"); // BUZZ
+    });
 
-  test('4) Fizzbuss 4th case: trick = fizzbuzz', () => {
-    const explorer = { name: "Explorer15", score: 15 }
-    const service = FizzbuzzService.applyValidationInExplorer(explorer)
-    expect(service).toBe('FIZZBUZZ') // FIZZBUZZ
-  })
+    test("4) Fizzbuss 4th case: trick = fizzbuzz", () => {
+        const explorer = { name: "Explorer15", score: 15 };
+        const service = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(service).toBe("FIZZBUZZ"); // FIZZBUZZ
+    });
 
-})
+});
