0d6eaedc3e4e886119ae4ab23cc5a5904ed03ccb
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-27 15:39:46 -0500

    19) Mejorando test para ExplorerService

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -4,7 +4,7 @@ describe("Test for ExplorerService", () => {
     test("1) Test: Filter by Mission", () => {
         const explorers = [{ mission: "node" }];
         const ExplorerByMission = ExplorerService.filterByMission(explorers, "node");
-        expect(ExplorerByMission.length).toBe(1);
+        expect(ExplorerByMission).toEqual(expect.arrayContaining([{ mission: "node" }]));
     });
 
     test("2) Test: Filter by Mission", () => {
