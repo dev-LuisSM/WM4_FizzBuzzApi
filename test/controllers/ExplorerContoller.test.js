fcef03de9b73aaddd6be4a8699bafa3f497fcbec
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-28 13:57:08 -0500

    24) Agregando test controller: Fizzbuzz y Fizzbuzz con número

--- test/controllers/ExplorerContoller.test.js
+++ test/controllers/ExplorerContoller.test.js
@@ -1,4 +1,5 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerContoller");
+const FizzBuzz = require("./../../lib/services/FizzbuzzService")
 
 describe("Test for ExplorerContoller", () => {
     test("1) Test: Get Explorer By Mission", () => {
@@ -17,5 +18,16 @@ describe("Test for ExplorerContoller", () => {
     test("3) Test: Get Explorers amount", () => {
         const ExplorersLenght = ExplorerController.getExplorersAmonutByMission("node");      
         expect(ExplorersLenght).toBe(10);
+    });
+
+    test("4) Test: Fizzbuzz", () => {
+      const explorer = {name: "explorer", score: 15}
+      const fzExplorer = FizzBuzz.applyValidationInExplorer(explorer);
+      expect(fzExplorer).toBe("FIZZBUZZ");
+    });
+
+    test("5) Test: Fizzbuzz with number", () => {
+      const fzNumber = FizzBuzz.applyValidationInNumber(5);
+      expect(fzNumber).toBe("BUZZ");
     });
 });
